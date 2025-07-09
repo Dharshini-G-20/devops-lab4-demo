47bb32cc51bbda08d32a0ace617396fdd13def31
Dharshini-G-20 <2021wa86221@wilp.bits-pilani.ac.in>
2025-07-10 00:13:29 +0530

    Update message to demonstrate CI

--- src/main/java/com/example/HelloWorld.java
+++ src/main/java/com/example/HelloWorld.java
@@ -2,10 +2,10 @@ package com.example;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, DevOps World!");
+        System.out.println("Hello, DevOps World! - Updated Version");
     }
 
     public String getMessage() {
-        return "Hello, DevOps World!";
+        return "Hello, DevOps World! - Updated Version";
     }
 }
